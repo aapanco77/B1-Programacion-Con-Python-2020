7a863d73c230779df7bd07042a9c5562e8bffe59
aapanco77 <alexiapanco@uci.edu.mx>
2022-04-19 21:57:03 -0500

    Commit 2 clase 1 de Python

--- Sesion-01/Ejemplo-03/operadores.py
+++ Sesion-01/Ejemplo-03/operadores.py
@@ -1,11 +1,29 @@
 # Vamos a ver como se comportan algunos operadores en Python
 # usando las siguientes variables
 
+from __future__ import division
+
+
 operando1 = 6
 operando2  = 10
 incremento = 0
 
 # Operadores aritmeticos
+suma = operando1 + operando2
+print( "La suma de los valores es: ")
+print(suma)
 
+producto = operando1 * operando2
+print( "El resultado de la multiplicación es: ")
+print(producto)
+
+division = operando1 / operando2
+print( "El resultado de la division es: ")
+print(division)
+
+incremento += 1 #incremento = incremento + 1 -> 1
+incremento += 2 #incremento = incremento + 2 -> 3
+print("El incremento es:")
+print(incremento)
 
 #Operadores lógicos
